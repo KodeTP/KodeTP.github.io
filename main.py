d3d52eab6afd5388496e3d1980617418cff8ef84
Taegon Hibbitts <thibbitts03@gmail.com>
2023-10-22 16:29:57 -0400

    initialize python script

---
+++ main.py
@@ -0,0 +1,17 @@
+page = "index.html"
+
+o = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Website</title>
+    </head>
+    <body>
+        <h1>My Website</h1>
+        <p>This is my website.</p>
+    </body>
+</html>
+"""
+
+with open(page, 'w', encoding='utf-8') as f:
+    f.write(o)
