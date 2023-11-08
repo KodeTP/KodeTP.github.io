8b3015857b18a4c59f161e6c91072d01158de05b
Taegon Hibbitts <thibbitts03@gmail.com>
2023-11-08 14:35:26 -0500

    erin and taegon slayed again

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import pandas as pd
 recipes = pd.read_csv("../RAW_recipes.csv")
 #print(recipes.head)
 
-ingredients = ['apples']
+ingredients = ['bok choy']
 
 page = "index.html"
 o = """
 <!DOCTYPE html>
 <html>
     <head>
-        <title>My Website</title>
+        <title>Recipie Finder</title>
     </head>
+    <div style="width: 397px; height: 62px; padding: 16px; background: white; border-radius: 16px; border: 1px #EFEFEF solid; justify-content: flex-start; align-items: center; gap: 10px; display: inline-flex">
+        <div style="color: #C5C5C5; font-size: 20px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Search...</div>
+    </div>
     <body>
-        <h1>My Website</h1>
-        <p>This is my website.</p> 
         <table>
             <tr>
                 <th>Name</th>
