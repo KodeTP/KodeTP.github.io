80b5dbe09e496cf6dfcfe885f1651b59e3dacead
Taegon Hibbitts <thibbitts03@gmail.com>
2023-11-08 14:35:16 -0500

    erin and taegon slayed

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
 recipes = pd.read_csv("../RAW_recipes.csv")
-print(recipes.head)
+#print(recipes.head)
+
+ingredients = ['apples']
 
 page = "index.html"
-
 o = """
 <!DOCTYPE html>
 <html>
@@ -13,7 +14,27 @@ o = """
     </head>
     <body>
         <h1>My Website</h1>
-        <p>This is my website.</p>
+        <p>This is my website.</p> 
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Ingredients</th>
+            </tr>"""
+
+for index, row in recipes.iterrows():
+    for ingredient in ingredients:
+        if ingredient in row['ingredients']:
+            o += """
+            <tr>
+                <td>{}</td>
+                <td>{}</td>
+            </tr>""".format(row['name'], row['ingredients'])
+            print(row['name'])
+            break
+
+
+
+o+= """
     </body>
 </html>
 """
